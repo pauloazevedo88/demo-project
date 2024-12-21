df666ade7a04b9ee7133f4fa9fa476340702973f
pauloazevedo88 <pauloazevedo-88@hotmail.com>
2024-12-21 21:42:08 +0000

    1st commit

---
+++ src/test/java/com/demo/paulo/first_demo_paulo/AppTest.java
@@ -0,0 +1,19 @@
+package com.demo.paulo.first_demo_paulo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
